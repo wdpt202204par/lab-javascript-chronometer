b03b7682ccaa795d83d8c9053763814e00cc96f6
unknown <Jossuha@hotmail.fr>
2022-05-17 20:54:08 +0200

    Finish

--- javascript/index.js
+++ javascript/index.js
@@ -2,8 +2,8 @@ const chronometer = new Chronometer();
 
 // get the DOM elements that will serve us to display the time:
 
-let btnLeftElement = document.getElementById('btnLeft')
-let btnRightElement = document.getElementById('btnRight')
+const btnLeftElement = document.getElementById('btnLeft')
+const btnRightElement = document.getElementById('btnRight')
 
 const minDecElement = document.getElementById('minDec'); // minute gauche
 const minUniElement = document.getElementById('minUni'); // minute droite
@@ -14,6 +14,8 @@ const milUniElement = document.getElementById('milUni');
 const splitsElement = document.getElementById('splits');
 
 let btnLeftState = false
+let startTime
+let msTimer
 
 // Start/Stop Button
 btnLeftElement.addEventListener('click', () => {
@@ -25,6 +27,7 @@ btnLeftElement.addEventListener('click', () => {
     btnRightElement.innerHTML = "RESET"
 
     chronometer.stop()
+    clearInterval(msTimer)
 
     btnLeftState = false
   }else { // Si le chrono est pas demarré -> Demarre le chrono
@@ -34,6 +37,8 @@ btnLeftElement.addEventListener('click', () => {
     btnRightElement.setAttribute("class", "btn split")
     btnRightElement.innerHTML = "SPLIT"
 
+    startTime = new Date().getTime();
+
     chronometer.start(function () { // Met à jour le chrono chaque secondes
       minDecElement.innerHTML = chronometer.computeTwoDigitNumber(chronometer.getMinutes())[0]
       minUniElement.innerHTML = chronometer.computeTwoDigitNumber(chronometer.getMinutes())[1]
@@ -41,6 +46,14 @@ btnLeftElement.addEventListener('click', () => {
       secDecElement.innerHTML = chronometer.computeTwoDigitNumber(chronometer.getSeconds())[0]
       secUniElement.innerHTML = chronometer.computeTwoDigitNumber(chronometer.getSeconds())[1]
     })
+
+    msTimer = setInterval(()=>{
+      const endTime = new Date().getTime();
+      let calcul = (endTime - startTime).toString()
+
+      milDecElement.innerHTML = calcul[calcul.length - 2]
+      milUniElement.innerHTML = calcul[calcul.length - 1]
+    },73)
 
     btnLeftState = true
   }
@@ -50,22 +63,23 @@ btnLeftElement.addEventListener('click', () => {
 btnRightElement.addEventListener('click', () => {
   
   if (btnLeftState) { // Si le chrono est demarré -> utilise le split
+    const endTime = new Date().getTime();
+    let calculMs = (endTime - startTime).toString()
+    calculMs = `${calculMs[calculMs.length - 3]}${calculMs[calculMs.length - 2]}${calculMs[calculMs.length - 1]}`
+    
     let split = document.createElement('li')
-    split.innerHTML = chronometer.split()
+    split.innerHTML = `${chronometer.split()}:${calculMs}`
     splitsElement.appendChild(split)
     
   } else { // Si le chrono n'est pas demarré -> Reset
     chronometer.reset()
-    minDecElement.innerHTML = 0
-    minUniElement.innerHTML = 0
-
-    secDecElement.innerHTML = 0
-    secUniElement.innerHTML = 0
+    
+    minDecElement.innerHTML = minUniElement.innerHTML = secDecElement.innerHTML = secUniElement.innerHTML = milDecElement.innerHTML = milUniElement.innerHTML = 0 // Reset du compteur HTML
 
     let splitList = document.getElementById("splits") // Dirige vers le 'ol' de split
-    //console.log(splitList.childElementCount)
+    let childCount = splitList.childElementCount // Compte le nombre de "li" presente dans la liste "Splits"
 
-    for (let i = 0; i < splitList.childElementCount; i++){ // Pour chaque element dans la liste de temps marqués
+    for (let i = 0; i < childCount; i++){ // Pour chaque element dans la liste de temps marqués
       splitList.removeChild(splitList.lastChild) // Supprime le dernier element dans la liste
     }
   }
